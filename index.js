36acc75c490bae9bb24cf521b235f96a745abc6c
plmqazoknijb <w2033@e-mirim.hs.kr>
2022-03-01 06:16:14 +0900

    연산자와 함수 실습

--- index.js
+++ index.js
@@ -28,14 +28,37 @@ console.log("pi_float의 값은", pi_float, "입니다." );
 console.log("쌍따옴표\"를 문자열 안에 쓸 수 있습니다.");
 console.log("문자열입니다.\n그리고 줄을 바꿔서 다음 문자열입니다.");
 
-//Object
+// Object
 var obj = {name:"송은원", age:"19"};
 
 var book = {"title":"어린왕자"};
 console.log("제목:", book.title);
 
-//undefined와 null
+// undefined와 null
 var a;  //undefined
 var b = null;   //null
 console.log("a의 자료형 :", typeof(a), "입니다.");
 console.log("b의 자료형 :", typeof(b), "이고, 값은", b, "입니다.");
+
+// 산술 연산자
+var a = 10;
+var b = 7;
+var c = a+b;    //a와 b의 합
+var d = a-b;    //a와 b의 차
+var e = a*b;    //a와 b의 곱
+console.log("a와 b의 합 :", c);
+console.log("a와 b의 차 :", d);
+console.log("a와 b의 곱 :", e);
+
+// 함수
+function solution1(a,b){
+    return a+b
+}
+
+// 관계연산자
+function solution2(a,b){
+    return a > b
+}
+function solution3(a,b){
+    return a && b
+}
